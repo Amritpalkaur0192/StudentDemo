8a9eeadb8e9771d00259712d39fb61441e303184
Amritpalkaur0192 <amritpalsanhu0192@gmail.com>
2019-01-17 13:21:39 -0500

    Initial Commit

---
+++ src/ca/software/week1student/Student.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.software.week1student;
+
+/**
+ *
+ * @author itsam
+ */
+public class Student {
+
+    /**
+     * @param args the command line arguments
+     */  private String name;
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+       
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
